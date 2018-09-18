88c8fa2585832812a342a0df15b1cba18000c58c
Dennis <dennisek85@gmail.com>
2018-09-18 10:52:05 +0200

    add scss file for recipe page

--- app.js
+++ app.js
@@ -7,28 +7,3 @@ const app = express();
 app.use(express.static('www'));
 // Start the web server on port 3000
 app.listen(3000,() => console.log('Listening on port 3000'));
-
-const mongoose = require('mongoose');
-
-/* mongoose.Promise = global.Promise;
-
-mongoose.connect("mongodb://localhost:27017/receptonaring", {
-  useNewUrlParser: true
-});
-
-let User = mongoose.model('User', {
-  email: String,
-  password: String
-});
-
-let user = new User({
-  email: "dennisek85@gmail.com",
-  password: 'test'
-});
-
-user.save().then((x)=>{
-    console.log('saved')
-  }, (e)=>{
-    console.log(e);
-  }
-) */
