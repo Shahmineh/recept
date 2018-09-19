d8cb04763f8bd05b198f1b382d6551612653c0da
PaulinaHT <paulina.hertor@gmail.com>
2018-09-19 14:10:06 +0200

    addRecipe template created

--- www/js/classes/app.class.js
+++ www/js/classes/app.class.js
@@ -1,12 +1,11 @@
-class App extends Base{
+class App {
     constructor(){
-        super();
         // Read data to our globals from json files
         this.livsmedelData;
         this.livsmedelDataIdHash = {};
         this.recipes;
-        this.clickEvents();
         this.start();
+        
         $.getJSON("/json/livsmedel.json", (data) => {
                 this.livsmedelData = data;
                 //filter the entire data to a smaller version
@@ -38,85 +37,12 @@ class App extends Base{
                 $.getJSON('/json/recept.json', (data) => {
                     this.recipes = data;
                     this.runTest();
+                    
                 })
             }
         );
         
     }
-
-    navigation(){
-        let navbar = new Navbar();
-        $('header').empty();
-        navbar.render('header');
-        // get the current url
-        let url = location.pathname;
-        
-        if (url == '/') {
-            let startsidan = new Startsidan(this);
-            $('main').empty();
-            startsidan.render('main');
-        }
-        if (url == '/recept') {
-            let recipe = new Recipe();
-            $('main').empty();
-            recipe.render('main');
-        }
-        if (url == '/huvudkategori') {
-            $('main').empty();
-            $('main').html(
-                `<h1>${url}</h1>
-                <h2>HTML/Render method here</h2>`
-            );
-        }
-        if (url == '/maltid') {
-            $('main').empty();
-            $('main').html(
-                `<h1>${url}</h1>
-                <h2>HTML/Render method here</h2>`
-            );
-        }
-        if (url == '/ingrediens') {
-            $('main').empty();
-            $('main').html(
-                `<h1>${url}</h1>
-                <h2>HTML/Render method here</h2>`
-            );            
-        }
-        if (url == '/specialkost') {
-            $('main').empty();
-            $('main').html(
-                `<h1>${url}</h1>
-                <h2>HTML/Render method here</h2>`
-            );            
-        }
-        if (url == '/varldens-mat') {
-            $('main').empty();
-            $('main').html(
-                `<h1>${url}</h1>
-                <h2>HTML/Render method here</h2>`
-            );            
-        }
-    }
-
-    clickEvents(){
-        let that = this;
-        //Add item
-        $(document).on("click", '#btn-add-allTasks', function() {
-            item.addItem(currentTab);
-        });
-        //Navigation
-        $(document).on('click','a.nav-btn',function(e){
-            //Create a push state preventDefault
-            let href = $(this).attr('href');
-            history.pushState(null, null, href);
-            //Call the change page function
-            that.navigation();
-            //Stop the browers from starting a page reload
-            e.preventDefault();
-        });
-    }
-
-
 
     // re-writes livesmedeldata for easier filtering
     createIdHashForLivsmedelData(){
@@ -135,7 +61,14 @@ class App extends Base{
     }
 
     start(){
-         
+        let navbar = new Navbar();
+        // let recipe = new Recipe();
+        let addRecipe = new AddRecipe();
+        $('header').empty();
+        $('main').empty();
+        navbar.render('header');
+        // recipe.render('header');
+        addRecipe.render('main');
     }
 
     
