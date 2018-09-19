bd19139a4345c8487b2f1f4f34d55066b1b19e10
André Tente <andretente@Andres-MacBook-Pro.local>
2018-09-19 17:03:02 +0200

    spa implemented

--- app.js
+++ app.js
@@ -19,5 +19,10 @@ const app = express();
 // Tell the web server to serve files
 // from the www folder
 app.use(express.static('www'));
+
+app.get(/^[^\.]*$/, (req, res) => {
+  res.sendFile(__dirname + '/www/index.html');
+});
+
 // Start the web server on port 3000
 app.listen(3000,() => console.log('Listening on port 3000'));
--- www/js/classes/app.class.js
+++ www/js/classes/app.class.js
@@ -1,10 +1,11 @@
-class App {
+class App extends Base{
     constructor(){
+        super();
         // Read data to our globals from json files
         this.livsmedelData;
         this.livsmedelDataIdHash = {};
         this.recipes;
-        
+        this.clickEvents();
         $.getJSON("/json/livsmedel.json", (data) => {
                 this.livsmedelData = data;
                 this.createIdHashForLivsmedelData();
@@ -17,6 +18,77 @@ class App {
         );
         
     }
+
+    navigation(){
+        // get the current url
+        let url = location.pathname;
+        
+        if (url == '/') {
+            let startsidan = new Startsidan(this);
+            $('main').empty();
+            startsidan.render('main');
+        }
+        if (url == '/recept') {
+            let recipe = new Recipe();
+            $('main').empty();
+            recipe.render('main');
+        }
+        if (url == '/huvudkategori') {
+            $('main').empty();
+            $('main').html(
+                `<h1>${url}</h1>
+                <h2>HTML/Render method here</h2>`
+            );
+        }
+        if (url == '/maltid') {
+            $('main').empty();
+            $('main').html(
+                `<h1>${url}</h1>
+                <h2>HTML/Render method here</h2>`
+            );
+        }
+        if (url == '/ingrediens') {
+            $('main').empty();
+            $('main').html(
+                `<h1>${url}</h1>
+                <h2>HTML/Render method here</h2>`
+            );            
+        }
+        if (url == '/specialkost') {
+            $('main').empty();
+            $('main').html(
+                `<h1>${url}</h1>
+                <h2>HTML/Render method here</h2>`
+            );            
+        }
+        if (url == '/varldens-mat') {
+            $('main').empty();
+            $('main').html(
+                `<h1>${url}</h1>
+                <h2>HTML/Render method here</h2>`
+            );            
+        }
+    }
+
+    clickEvents(){
+        let that = this;
+        //Add item
+        $(document).on("click", '#btn-add-allTasks', function() {
+            item.addItem(currentTab);
+        });
+        //Navigation
+        $(document).on('click','a.nav-btn',function(e){
+            //Create a push state preventDefault
+            let href = $(this).attr('href');
+            history.pushState(null, null, href);
+            //Call the change page function
+            that.navigation();
+            //Stop the browers from starting a page reload
+            e.preventDefault();
+        });
+    }
+
+
 
     // re-writes livesmedeldata for easier filtering
     createIdHashForLivsmedelData(){
@@ -36,17 +108,14 @@ class App {
 
     start(){
         let navbar = new Navbar();
-        let recipe = new Recipe();
+        
         $('header').empty();
-        $('main').empty();
+        
         navbar.render('header');
 
 
 
-        // Create pages
-        let startsidan = new Startsidan(this);
-        $('main').empty();
-        startsidan.render('main');
+        
     }
 
     
