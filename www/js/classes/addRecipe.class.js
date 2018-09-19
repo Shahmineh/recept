2aea479bded9b4dbe3dcd7e88e6b15d5fbc69f69
PaulinaHT <paulina.hertor@gmail.com>
2018-09-19 14:10:39 +0200

    Method AddRecipe in progress js

---
+++ www/js/classes/addRecipe.class.js
@@ -0,0 +1,14 @@
+class AddRecipe extends Base {
+  constructor(){
+    super();
+    this.addRecipe();
+  }
+
+addRecipe(){
+    $(document).on('click', '#submit-btn', function(){
+    let recipeName = $('#recipe-name').val();
+  })
+}
+
+}
+
