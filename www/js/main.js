7a37fe3849009146c4a74e294e81042af334b3e4
André Tente <andretente@Andres-MacBook-Pro.local>
2018-09-21 11:03:32 +0200

    fix of multiple class instances

--- www/js/main.js
+++ www/js/main.js
@@ -4,18 +4,18 @@ app.navigation();
 window.addEventListener('popstate',app.navigation);
 
 
-const categories = $.getJSON("/json/categories.json",
-    function (data) {
-        const categoryList = Object.keys(data).map(category => {
-            //Create sub category list
-            const subCategories = data[category].map(subCategory => {
-                return `
-                    <li class="nav-item">
-                        <a class="nav-link active btn-category" href="#">${subCategory}</a>
-                    </li>`;
-            }).join('');    
-        });
-    })
+// const categories = $.getJSON("/json/categories.json",
+//     function (data) {
+//         const categoryList = Object.keys(data).map(category => {
+//             //Create sub category list
+//             const subCategories = data[category].map(subCategory => {
+//                 return `
+//                     <li class="nav-item">
+//                         <a class="nav-link active btn-category" href="#">${subCategory}</a>
+//                     </li>`;
+//             }).join('');    
+//         });
+//     })
 
 // const categories = $.getJSON("/json/categories.json",
 //     function (data) {
