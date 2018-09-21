da706add86101058499b7e9c59dcb7c027470157
André Tente <andretente@Andres-MacBook-Pro.local>
2018-09-21 23:28:49 +0200

    search feature implemented

---
+++ www/js/classes/search.class.js
@@ -0,0 +1,34 @@
+class Search{
+    constructor(recipes, ingredients){
+        this.recipes = recipes;
+        this.ingredients = ingredients;
+        this.filter = new Filter(this.ingredients, this.recipes);
+        this.eventHandler();
+    }
+    eventHandler(){
+        let that = this;
+        $(document).on('click', '#search-button', function(){
+            let val = $('#search-input').val();
+           // console.log(that.filterRecipes(val));
+            //that.filterRecipes(val);
+        });
+        $(document).on('keydown', '#search-input', function(event){
+            let val = $('#search-input').val();
+            if(event.keyCode === 13){
+                event.preventDefault();
+
+                // console.log(that.filter.filterIngredients(val));
+
+                let searchResult = that.filter.filterRecipes(val).length ? that.filter.filterRecipes(val) : that.filter.filterIngredients(val).map(item=>{
+                    return that.filter.filterRecipes(item)
+                }).filter(item=>item.length).flat();
+
+                console.log('search result',searchResult);
+
+                $('#search-input').val('');
+            }
+        });
+    }
+
+    
+}
