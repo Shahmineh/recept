aa8dafca05368f19f8e9a63848f4ff7a78ac3d70
André Tente <andretente@Andres-MacBook-Pro.local>
2018-09-22 12:10:46 +0200

    add ingredient auto-complete

--- www/js/classes/app.class.js
+++ www/js/classes/app.class.js
@@ -13,7 +13,7 @@ class App extends Base{
                 $.getJSON('/json/recipe.json', (data) => {
                     this.recipes = data;
                     this.runTest();
-                    this.addRecipe = new AddRecipe(this.recipes, this.livsmedelData);
+                    this.addRecipe = new AddRecipe(this.recipes, this.livsmedelDataIdHash);
                     this.search = new Search(this.recipes, this.livsmedelData);
                     this.navigation();
                 })
