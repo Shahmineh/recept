93f6115b5ee2e883b818c69831359a0ed4ff288a
Dennis <dennisek85@gmail.com>
2018-09-25 14:21:31 +0200

    add dynamic rendering for ingredient and instruction list

--- www/js/classes/recipe.class.js
+++ www/js/classes/recipe.class.js
@@ -1,5 +1,26 @@
 class Recipe extends Base {
-  constructor(){
+  constructor(filter, filterString){
     super();
+    this.recipe = filter.filterRecipes(filterString)[0];
   }
+
+  ingredientList(){
+    let that = this;
+    this.recipe.ingredients.map( ingredient => {
+      console.log(ingredient);
+      return $(".ingredient-control").append(`<li class="ingredient-layout">${ingredient.amount} ${ingredient.unit} ${ingredient.number}</li>`);
+    });
+    
+    
+
+  }
+
+  instructionList(){
+    let that = this;
+    Object.values(this.recipe.steps).map( instruction => {
+      return $(".recipe").append(`<li class="recipe-layout">${instruction}</li>`);
+    });
+  }
+
+
 }
